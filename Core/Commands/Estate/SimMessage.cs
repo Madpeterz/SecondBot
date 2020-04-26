d1af896a652792f2f4c797b01acb3a7fd6bfc6b5
Madpeter <madpeter@gmail.com>
2020-04-26 16:54:25 +0100

    new command: SimMessage

---
+++ Core/Commands/Estate/SimMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BetterSecondBotShared.logs;
+using BetterSecondBotShared.Static;
+using OpenMetaverse;
+
+namespace BSB.Commands.Estate
+{
+    public class SimMessage : CoreCommand
+    {
+        public override string[] ArgTypes { get { return new[] { "Text" }; } }
+        public override string[] ArgHints { get { return new[] { "Message to send" }; } }
+        public override int MinArgs { get { return 1; } }
+        public override string Helpfile { get { return "Sends the message [ARG 1] to the current sim"; } }
+
+        public override bool CallFunction(string[] args)
+        {
+            if (base.CallFunction(args) == true)
+            {
+                if (bot.GetClient.Network.CurrentSim.IsEstateManager == true)
+                {
+                    bot.GetClient.Estate.SimulatorMessage(args[0]);
+                    return true;
+                }
+                else
+                {
+                    return Failed("Not an estate manager here");
+                }
+            }
+            return false;
+        }
+    }
+}
