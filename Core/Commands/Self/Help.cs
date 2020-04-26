298872acc821764f86a30cde1047a2cbd23e3fca
Tikuf <lucas@snowleopard.pro>
2020-04-25 20:52:06 -0600

    Expanded help file

--- Core/Commands/Self/Help.cs
+++ Core/Commands/Self/Help.cs
@@ -10,17 +10,21 @@ namespace BSB.Commands.Self
     public class Help : CoreCommand
     {
 
-        public override string Helpfile { get { return "A Full list of supported commands"; } }
+        public override string Helpfile { get { return "Basic list of commands"; } }
         public override bool CallFunction(string[] args)
         {
             if (bot.getMaster_uuid != UUID.Zero)
             {
               
                 StringBuilder sb = new StringBuilder();
-                sb.Append(bot.GetCommandsInterface.GetCommandHelp("Im"));
-                sb.Append(bot.GetCommandsInterface.GetCommandHelp("Say"));
-                sb.Append(bot.GetCommandsInterface.GetCommandHelp("BotSit"));
-                sb.Append("Type: 'morehelp' for a full list of commands");
+                sb.Append(bot.GetCommandsInterface.GetCommandHelp("im"));
+                sb.Append("\n\n");
+                sb.Append(bot.GetCommandsInterface.GetCommandHelp("say"));
+                sb.Append("\n\n");
+                sb.Append(bot.GetCommandsInterface.GetCommandHelp("botsit"));
+                sb.Append("\n\n");
+                sb.Append("\nType: 'morehelp' for a full list of commands");
+                sb.Replace("<br/>", "\n");
                 bot.sendIM(bot.getMaster_uuid, sb.ToString());
 
                 return true;
