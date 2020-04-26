813aa5a330cf265847f6a5d696d5b325239177b1
Tikuf <lucas@snowleopard.pro>
2020-04-25 19:24:03 -0600

    Great new help

---
+++ Core/Commands/Self/Help.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using OpenMetaverse;
+
+
+namespace BSB.Commands.Self
+{ 
+    public class Help : CoreCommand
+    {
+     
+        public override string Helpfile { get { return "Makes the bot turn to face [ARG 1] and point at them (if found)"; } }
+        public override bool CallFunction(string[] args)
+        {
+            if (UUID.TryParse(bot, out UUID testavatar) == true)
+            {
+
+            }
+            else
+            {
+                return Failed("UUID is not vaild");
+            }
+        }
+    }
+}
