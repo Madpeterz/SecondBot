321e3f67dbd79779ea6e7bc6d51ee0eef89d5621
Madpeter <madpeter@gmail.com>
2020-05-06 17:34:44 +0100

    New command: Balance returns the bots L$ value via smart target.

---
+++ Core/Commands/Self/Balance.cs
@@ -0,0 +1,17 @@
+﻿namespace BSB.Commands.Self
+{
+    public class Balance : CoreCommand_SmartReply_1arg
+    {
+        public override string Helpfile { get { return "Requests the current balance and sends that via the smart reply target and then requests the balance to update."; } }
+        public override bool CallFunction(string[] args)
+        {
+            if(bot.GetAllowFunds == true)
+            {
+                bool status = bot.GetCommandsInterface.SmartCommandReply(args[0], bot.GetClient.Self.Balance.ToString(), CommandName);
+                bot.GetClient.Self.RequestBalance();
+                return status;
+            }
+            return bot.GetCommandsInterface.SmartCommandReply(args[0], "0", CommandName);
+        }
+    }
+}
