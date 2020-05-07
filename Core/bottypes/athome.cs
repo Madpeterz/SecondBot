145bebcda3eb946d7828549be2e07651e1954b1f
Madpeter <madpeter@gmail.com>
2020-05-07 04:31:40 +0100

    removes some unneed using from athome bot type

--- Core/bottypes/athome.cs
+++ Core/bottypes/athome.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 using OpenMetaverse;
-using OpenMetaverse.Assets;
 using BetterSecondBotShared.Static;
 using BetterSecondBotShared.logs;
 
