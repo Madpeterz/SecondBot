3b7d955cbd1d70def0a7134b595fced620244d90
Madpeter <madpeter@gmail.com>
2020-05-07 08:30:41 +0100

    new command: SimName

---
+++ Core/Commands/Info/SimName.cs
@@ -0,0 +1,15 @@
+﻿namespace BSB.Commands.Info
+{
+    public class SimName : CoreCommand_SmartReply_1arg
+    {
+        public override string Helpfile { get { return "Requests the current Sim Position and sends that via the smart reply target"; } }
+        public override bool CallFunction(string[] args)
+        {
+            if (base.CallFunction(args) == true)
+            {
+                return bot.GetCommandsInterface.SmartCommandReply(args[0], bot.GetClient.Network.CurrentSim.Name, CommandName);
+            }
+            return false;
+        }
+    }
+}
