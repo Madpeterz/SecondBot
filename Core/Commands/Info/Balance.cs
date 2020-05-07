6fc33d558c8b7c297283ce6b6a1ca4bad1e5d533
Madpeter <madpeter@gmail.com>
2020-05-07 08:27:48 +0100

    Moves the Balance command into the info namespace

---
+++ Core/Commands/Info/Balance.cs
@@ -0,0 +1,21 @@
+﻿namespace BSB.Commands.Info
+{
+    public class Balance : CoreCommand_SmartReply_1arg
+    {
+        public override string Helpfile { get { return "Requests the current balance and sends that via the smart reply target and then requests the balance to update."; } }
+        public override bool CallFunction(string[] args)
+        {
+            if (base.CallFunction(args) == true)
+            {
+                if (bot.GetAllowFunds == true)
+                {
+                    bool status = bot.GetCommandsInterface.SmartCommandReply(args[0], bot.GetClient.Self.Balance.ToString(), CommandName);
+                    bot.GetClient.Self.RequestBalance();
+                    return status;
+                }
+                return bot.GetCommandsInterface.SmartCommandReply(args[0], "0", CommandName);
+            }
+            return false;
+        }
+    }
+}
