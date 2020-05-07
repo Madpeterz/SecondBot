099580928a9ad417070a18cd6e43488f8bcf57f8
Madpeter <madpeter@gmail.com>
2020-05-07 08:29:41 +0100

    new command: GetPosition

---
+++ Core/Commands/Info/GetPosition.cs
@@ -0,0 +1,15 @@
+﻿namespace BSB.Commands.Info
+{
+    public class GetPosition : CoreCommand_SmartReply_1arg
+    {
+        public override string Helpfile { get { return "Requests the Position and sends that via the smart reply target"; } }
+        public override bool CallFunction(string[] args)
+        {
+            if (base.CallFunction(args) == true)
+            {
+                return bot.GetCommandsInterface.SmartCommandReply(args[0], bot.GetClient.Self.RelativePosition.ToString(), CommandName);
+            }
+            return false;
+        }
+    }
+}
