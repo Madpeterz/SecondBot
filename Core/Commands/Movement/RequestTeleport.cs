9a4cdda1497006731098e14ca6800aae1374523d
Madpeter <madpeter@gmail.com>
2020-05-11 12:12:48 +0100

    new command: RequestTeleport

---
+++ Core/Commands/Movement/RequestTeleport.cs
@@ -0,0 +1,26 @@
+﻿using OpenMetaverse;
+
+namespace BSB.Commands.Movement
+{
+    public class RequestTeleport : CoreCommand_1arg
+    {
+        public override string[] ArgTypes { get { return new[] { "Avatar" }; } }
+        public override string[] ArgHints { get { return new[] { "Avatar UUID or Firstname Lastname" }; } }
+        public override string Helpfile { get { return "Sends a teleport lure request (Move the bot to the avatar)"; } }
+
+        public override bool CallFunction(string[] args)
+        {
+            if(base.CallFunction(args) == true)
+            {
+                if(UUID.TryParse(args[0],out UUID avatar) == true)
+                {
+                    bot.add_uuid_to_teleport_list(avatar);
+                    bot.GetClient.Self.SendTeleportLureRequest(avatar, "I would like to teleport to you");
+                    return true;
+                }
+                return Failed("Unable to process avatar on arg 1");
+            }
+            return false;
+        }
+    }
+}
