09584304718562e0f61fea113721e37c555bbca8
Madpeter <madpeter@gmail.com>
2020-05-15 23:23:04 +0100

    WebUI files

--- CLI/HttpWebUi/theme/js/site.js
+++ CLI/HttpWebUi/theme/js/site.js
@@ -1,4 +1,56 @@
 $( document ).ready(function() {
+    $(".ajaxq").submit(function(e) {
+        e.preventDefault();
+        var form = $(this);
+        var url = form.attr('action');
+        var method = form.attr('method');
+        $.ajax({
+               type: method,
+               url: url,
+               data: form.serialize(),
+               success: function(data)
+               {
+                   try
+                   {
+                       jsondata = JSON.parse(data);
+                       var redirectdelay = 1500;
+                       if(jsondata.hasOwnProperty('status'))
+                       {
+                           if(jsondata.hasOwnProperty('message'))
+                           {
+                               if(jsondata.status == false)
+                               {
+                                   redirectdelay = 3500;
+                                   if(jsondata.message != "") alert_warning(jsondata.message);
+                               }
+                           }
+                           if(jsondata.hasOwnProperty('redirect'))
+                           {
+                               if(jsondata.redirect != "")
+                               {
+                                   if(jsondata.redirect != null)
+                                   {
+                                    setTimeout(function(){ $(location).attr('href', jsondata.redirect) }, redirectdelay);
+                                    }
+                               }
+                           }
+                       }
+                       else
+                       {
+                           alert_error("Reply from server is not vaild please reload the page and try again.");
+                       }
+                   }
+                   catch(e)
+                   {
+                       alert_warning("Unable to process reply, please reload the page and try again!");
+                   }
+               },
+               error: function (data)
+               {
+                   alert_error(data);
+               }
+        });
+    });
     $(".ajax").submit(function(e) {
         e.preventDefault();
         var form = $(this);
@@ -77,5 +129,73 @@ function alert(smsg,alerttype)
     },{
     	// settings
     	type: alerttype
+    });
+}
+
+function update_localchat(urlbase)
+{
+    $.ajax({
+        type: "post",
+        url: ""+urlbase+"ajax/localchat/get",
+        success: function(data)
+        {
+            try
+            {
+                jsondata = JSON.parse(data);
+                var redirectdelay = 1500;
+                if(jsondata.hasOwnProperty('status'))
+                {
+                    if(jsondata.hasOwnProperty('message'))
+                    {
+                        if(jsondata.status == true)
+                        {
+                            $('#localchat').prop('readonly',false);
+                            var text = "";
+                            var addon = "";
+                            $.each( JSON.parse(jsondata.message), function( key, value ) {
+                              text = ""+value+""+addon+""+text+"";
+                              addon = "\n";
+                            });
+                            $('#localchat').val(text);
+                            $('#localchat').prop('readonly',true);
+						}
+                    }
+                }
+            }
+            catch(e)
+            {
+                alert_warning("Unable to process reply, please reload the page and try again!");
+            }
+        }
+    });
+}
+function update_groupchat(urlbase,groupuuid)
+{
+    $.ajax({
+        type: "post",
+        url: ""+urlbase+"ajax/groupchat/get",
+        data: "groupuuid="+groupuuid+"",
+        success: function(data)
+        {
+            try
+            {
+                jsondata = JSON.parse(data);
+                var redirectdelay = 1500;
+                if(jsondata.hasOwnProperty('status'))
+                {
+                    if(jsondata.hasOwnProperty('message'))
+                    {
+                        if(jsondata.status == true)
+                        {
+                            $("localchat").val(jsondata.message);
+						}
+                    }
+                }
+            }
+            catch(e)
+            {
+                alert_warning("Unable to process reply, please reload the page and try again!");
+            }
+        }
     });
 }
