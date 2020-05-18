c3e4af6658a93d60f9c513e26596d78458d80b0d
Madpeter <madpeter@gmail.com>
2020-05-18 15:49:36 +0100

    new command: GetSimGlobalPos

---
+++ Core/Commands/Estate/GetSimGlobalPos.cs
@@ -0,0 +1,27 @@
+﻿using OpenMetaverse;
+
+namespace BSB.Commands.Estate
+{
+    public class GetSimGlobalPos : CoreCommand_SmartReply_2arg
+    {
+        public override string Helpfile { get { return "Fetchs a region and returns its global X,Y"; } }
+
+        public override bool CallFunction(string[] args)
+        {
+            if (base.CallFunction(args) == true)
+            {
+                if (bot.GetClient.Grid.GetGridRegion(args[1], GridLayerType.Objects, out GridRegion region))
+                {
+                    collection.Add("X", region.X.ToString());
+                    collection.Add("Y", region.Y.ToString());
+                    return bot.GetCommandsInterface.SmartCommandReply(true, args[0], "ok", CommandName,collection);
+                }
+                else
+                {
+                    return Failed("Unable to find region");
+                }
+            }
+            return false;
+        }
+    }
+}
