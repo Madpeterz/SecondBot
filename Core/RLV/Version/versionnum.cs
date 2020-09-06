39359368f65afbcb2032282666d000ee8bc81dc5
Madpeter <madpeter@gmail.com>
2020-09-07 00:00:23 +0100

    RLV patch for version commands, now if you dont define a channel it will default to zero

--- Core/RLV/Version/versionnum.cs
+++ Core/RLV/Version/versionnum.cs
@@ -4,17 +4,24 @@ using System.Text;
 
 namespace BSB.RLV.Version
 {
-    public class VersionNum : RLV_command_1arg
+    public class VersionNum : RLV_command
     {
         public override bool CallFunction(string[] args)
         {
-            _ = int.TryParse(args[0], out int channel);
+            int channel = 0;
+            if (args.Length == 1)
+            {
+                _ = int.TryParse(args[0], out channel);
+            }
             if (channel >= 0)
             {
                 bot.GetClient.Self.Chat("209000", channel, OpenMetaverse.ChatType.Normal);
                 return true;
             }
-            return Failed("Invaild channel given");
+            else
+            {
+                return Failed("Invaild channel given");
+            }
         }
     }
 }
