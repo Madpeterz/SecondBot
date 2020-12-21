041668a0ed10101bab180fe0d6104d9c97241b3d
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2020-12-21 11:29:33 +0000

    Revert "change pos for nearme to a json encoded vector3"
    This reverts commit 23211866519128de006a12ee09de27011d160827.

--- Core/Commands/Avatars/NearMe.cs
+++ Core/Commands/Avatars/NearMe.cs
@@ -24,7 +24,7 @@ namespace BSB.Commands.Avatars
                     }
                     Dictionary<string, string> entrys = new Dictionary<string, string>();
                     entrys.Add("Username",av.Name);
-                    entrys.Add("Pos", JsonConvert.SerializeObject(pos));
+                    entrys.Add("Pos", pos.X.ToString() + "," + pos.Y.ToString() + "," + pos.Z.ToString());
                     collection.Add(av.ID.ToString(), JsonConvert.SerializeObject(entrys));
                 }
                 return bot.GetCommandsInterface.SmartCommandReply(true, args[0], "ok", CommandName, collection);
