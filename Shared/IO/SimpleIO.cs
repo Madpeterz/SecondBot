f3ab320a28ff848797de427a70f06795f68e6293
Madpeterz <57852827+Madpeterz@users.noreply.github.com>
2021-05-18 06:21:10 +0100

    Update SimpleIO.cs
    improves IO controler

--- Shared/IO/SimpleIO.cs
+++ Shared/IO/SimpleIO.cs
@@ -14,90 +14,76 @@ namespace BetterSecondBotShared.IO
         public void ChangeRoot(string A)
         {
 
-            root_folder = A + "/";
-            create_dir(root_folder);
-        }
-        public void create_dir(string target_folder)
-        {
-            if (dir_exists(root_folder) == false)
+            root_folder = A;
+            if (DirExists(root_folder) == false)
             {
                 System.IO.Directory.CreateDirectory(root_folder);
             }
         }
-        public static bool dir_exists(string target_folder)
+        public static bool DirExists(string target_folder)
         {
             return System.IO.Directory.Exists(target_folder);
         }
         public void Delete(string filename)
         {
-            System.IO.File.Delete(root_folder+filename);
+            System.IO.File.Delete(root_folder+"/"+filename);
         }
-        public void writefile(string filename,string content)
+        public void WriteFile(string filename,string content)
         {
-            if(Exists(filename))
+            if(Exists(filename) == true)
             {
                 Delete(filename);
             }
-            System.IO.File.WriteAllText(@""+root_folder + "" + filename, content);
+            System.IO.File.WriteAllText(@""+root_folder + "/" + filename, content);
         }
         public void WriteJsonConfig(JsonConfig Config, string targetfile)
         {
             JsonSerializer serializer = new JsonSerializer();
-            using (StreamWriter sw = new StreamWriter(@""+root_folder + "" + targetfile))
-            using (JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented })
-            {
-                serializer.Serialize(writer, Config);
-            }
+            using StreamWriter sw = new StreamWriter(@"" + root_folder + "/" + targetfile);
+            using JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented };
+            serializer.Serialize(writer, Config);
         }
         public void WriteJsonCommands(JsonCommandsfile commandsfile, string targetfile)
         {
             JsonSerializer serializer = new JsonSerializer();
-            using (StreamWriter sw = new StreamWriter(@"" + root_folder + "" + targetfile))
-            using (JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented })
-            {
-                serializer.Serialize(writer, commandsfile);
-            }
+            using StreamWriter sw = new StreamWriter(@"" + root_folder + "/" + targetfile);
+            using JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented };
+            serializer.Serialize(writer, commandsfile);
         }
 
         public void WriteJsonTokens(JsonScopedTokens tokensfile, string targetfile)
         {
             JsonSerializer serializer = new JsonSerializer();
-            using (StreamWriter sw = new StreamWriter(@"" + root_folder + "" + targetfile))
-            using (JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented })
-            {
-                serializer.Serialize(writer, tokensfile);
-            }
+            using StreamWriter sw = new StreamWriter(@"" + root_folder + "/" + targetfile);
+            using JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented };
+            serializer.Serialize(writer, tokensfile);
         }
 
         public void WriteJsonRelays(JsonCustomRelaysSet relays, string targetfile)
         {
             JsonSerializer serializer = new JsonSerializer();
-            using (StreamWriter sw = new StreamWriter(@"" + root_folder + "" + targetfile))
-            using (JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented })
-            {
-                serializer.Serialize(writer, relays);
-            }
+            using StreamWriter sw = new StreamWriter(@"" + root_folder + "/" + targetfile);
+            using JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented };
+            serializer.Serialize(writer, relays);
         }
 
         public void WriteJsonAdverts(advertsBlob adverts, string targetfile)
         {
             JsonSerializer serializer = new JsonSerializer();
-            using (StreamWriter sw = new StreamWriter(@"" + root_folder + "" + targetfile))
-            using (JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented })
-            {
-                serializer.Serialize(writer, adverts);
-            }
+            using StreamWriter sw = new StreamWriter(@"" + root_folder + "/" + targetfile);
+            using JsonWriter writer = new JsonTextWriter(sw) { Formatting = Formatting.Indented };
+            serializer.Serialize(writer, adverts);
         }
 
 
 
-        public void makeOld(string targetfile)
+        public void MarkOld(string targetfile)
         {
-            System.IO.File.Move(@""+root_folder + "" + targetfile, @"" + root_folder + "" + targetfile+".old");
+            System.IO.File.Move(@""+root_folder + "/" + targetfile, @"" + root_folder + "/" + targetfile+".old");
         }
         public bool Exists(string targetfile)
         {
-            return File.Exists(@""+root_folder+"" + targetfile);
+            return File.Exists(@""+root_folder+"/" + targetfile);
         }
         public static bool FileType(string targetfile, string matchtype)
         {
@@ -113,7 +99,7 @@ namespace BetterSecondBotShared.IO
             if (Exists(targetfile) == true)
             {
                 string return_text = "";
-                using (var Stream = new FileStream(@"" + root_folder + "" + targetfile, FileMode.Open, FileAccess.Read))
+                using (var Stream = new FileStream(@"" + root_folder + "/" + targetfile, FileMode.Open, FileAccess.Read))
                 using (var StreamReader = new StreamReader(Stream))
                 {
                     return_text = StreamReader.ReadToEnd();
